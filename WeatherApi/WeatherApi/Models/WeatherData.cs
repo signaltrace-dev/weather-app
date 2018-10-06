4a2aa7461cea6089b2b1fa6f1f85788ce7f21888
Jeff Glenn <jglenn@mergeworld.com>
2018-10-06 07:54:15 -0400

    Add models

---
+++ WeatherApi/WeatherApi/Models/WeatherData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApi.Models
+{
+    public class WeatherData
+    {
+        public string CityName { get; set; }
+        public string Description { get; set; }
+        public int Humidity { get; set; }
+        public decimal Temperature { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ WeatherApi/WeatherApi/Models/WeatherDataRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherApi.Models
+{
+    public class WeatherDataRequest
+    {
+        [Required(ErrorMessage = "Location is required.")]
+        [StringLength(200, ErrorMessage = "Location cannot be longer than 200 characters.")]
+        public string Location { get; set; }
+        public DateTime Timestamp { get; }
+
+        public WeatherDataRequest()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
